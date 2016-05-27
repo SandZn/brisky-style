84cb9d5c4b04969fb1e7b99759a3faee436f0300
Youri Daamen <me@youz.io>
2016-05-27 10:26:33 +0200

    added tests for state true

--- test/index.js
+++ test/index.js
@@ -2,13 +2,11 @@
 require('brisky-core').prototype.inject(require('../'))
 
 const test = require('tape')
+const s = require('vigour-state/s')
 const render = require('brisky-core/render')
 
 test('static styles', function (t) {
-  var elem
-  t.plan(2)
-
-  elem = render({
+  var elem = render({
     style: {
       padding: '100px'
     }
@@ -27,4 +25,43 @@ test('static styles', function (t) {
     (elem.style.padding === '100px') &&
     (elem.style.margin === '50px'),
     'add multiple styles')
+
+  t.end()
+})
+
+test('state styles', function (t) {
+  var elem = render({
+    style: {
+      display: {
+        $: 'thing'
+      }
+    }
+  }, {
+    thing: 'none'
+  })
+
+  t.equals(elem.style.display, 'none', 'add display property using state')
+
+  const state = s({
+    thing: 'none'
+  })
+
+  elem = render({
+    $: 'thing',
+    style: {
+      display: {
+        $: true
+      }
+    }
+  }, state)
+
+  t.equals(elem.style.display, 'none', 'add display property using state true')
+
+  state.set({
+    thing: 'block'
+  })
+
+  t.equals(elem.style.display, 'block', 'add display property using state true, update')
+
+  t.end()
 })
