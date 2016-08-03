badbca201be232cf14279b36795931b31a5d5d1b
Jim de Beer <jim@vigour.io>
2016-08-03 14:06:58 +0200

    added newline

---
+++ test/context.js
@@ -0,0 +1,22 @@
+  'use strict'
+
+  // types: {
+  //   dimensions: {
+  //     style: {
+  //       border: '3px solid white',
+  //       width: {
+  //         $: 'width',
+  //         $transform: (val) => val * 0.05
+  //       },
+  //       height: {
+  //         $: 'height',
+  //         $transform: (val) => val * 0.05
+  //       },
+  //     }
+  //   }
+  // },
+  // child: {
+  //   class: true,
+  //   child: 'Constructor'
+  // },
+  // // dimensions: {  type: 'dimensions', $: 'client' },
