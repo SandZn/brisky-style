0896032a85c602ca1ab1cb642baab17235dd6e5d
Jim de Beer <jim@vigour.io>
2016-09-30 12:29:23 +0200

    added extra test, addde bubleify

--- test/transform.js
+++ test/transform.js
@@ -36,3 +36,30 @@ test('transform - state', function (t) {
   t.equals(elem.style.transform, 'translate3d(-5px, 5px, 0px) scale(0.1) rotate(5deg)', 'mixed state and static')
   t.end()
 })
+
+test('transform - state - remove', function (t) {
+  const state = s({
+    field: {
+      x: -5,
+      y: 5,
+      rot: 5
+    }
+  })
+  const elem = render({
+    field: {
+      $: 'field',
+      style: {
+        transform: {
+          y: { $: 'y' },
+          x: { $: 'x' },
+          rotate: { $: 'rot' },
+          scale: 0.1
+        }
+      }
+    }
+  }, state)
+  t.equals(elem.childNodes[0].style.transform, 'translate3d(-5px, 5px, 0px) scale(0.1) rotate(5deg)', 'intial')
+  state.field.remove()
+  t.ok(true, 'should not crash on remove')
+  t.end()
+})
